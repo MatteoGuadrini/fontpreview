ed885e2a71e4bc3878febbe25cca8771769e0a0b
Matteo Guadrini <matteo.guadrini@hotmail.it>
2020-11-23 10:37:20 +0100

    Rewrite __init__ method of FontPreview class

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from distutils.core import setup
+from setuptools import setup
 VERSION = '0.8.0'
 
 with open("README.md", "r") as fh:
